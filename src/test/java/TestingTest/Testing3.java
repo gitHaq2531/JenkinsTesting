53d578483d6ca5a10091cfe3ceefd45d77717dc0
gitHaq2531 <afzaulhaque1@gmail.com>
2025-07-15 17:13:49 +0530

    m03

--- src/test/java/TestingTest/Testing3.java
+++ src/test/java/TestingTest/Testing3.java
@@ -7,11 +7,8 @@ public class Testing3 {
 	@Test
 	public void simpleTest()
 	{
-<<<<<<< HEAD
 		System.out.println("Simple Test executed");
 		System.out.println("simple test executed 2");
-=======
 		System.out.println("Simple Test exdecuted");
->>>>>>> master
 	}
 }
