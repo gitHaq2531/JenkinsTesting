0a2bd35defd91b64f6a8268dcc7406cd6c668bc1
aaratiInamdar <arti.sanas3555@gmail.com>
2025-07-15 15:02:49 +0530

    commit1

---
+++ src/test/java/TestingTest/Testing3.java
@@ -0,0 +1,12 @@
+package TestingTest;
+
+import org.testng.annotations.Test;
+
+public class Testing3 {
+
+	@Test
+	public void simpleTest()
+	{
+		System.out.println("Simple Test exdecuted");
+	}
+}
