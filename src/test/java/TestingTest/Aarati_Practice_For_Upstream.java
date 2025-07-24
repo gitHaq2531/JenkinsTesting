218083fbd20368093b2b60b57e5dd686177fd8ed
aaratiInamdar <arti.sanas3555@gmail.com>
2025-07-24 17:48:19 +0530

    commitfromaarati1

--- src/test/java/TestingTest/Aarati_Practice_For_Upstream.java
+++ src/test/java/TestingTest/Aarati_Practice_For_Upstream.java
@@ -13,8 +13,8 @@ public class Aarati_Practice_For_Upstream {
 	@Test
 	public void simpleTest2()
 	{
-	
+	System.out.println("hello");
 		System.out.println("todays practice 19-07-2025");
-		System.out.println("my update");
+		System.out.println("todays update from aarati 24-07-2025");
 	}
 }
