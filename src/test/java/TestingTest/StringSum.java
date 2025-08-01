2a392a0095750ba707537a66ed692904ee23267f
shewaleakshay329 <akshayshewale329@gmail.com>
2025-08-01 15:14:16 +0530

    commit1aug2

--- src/test/java/TestingTest/StringSum.java
+++ src/test/java/TestingTest/StringSum.java
@@ -3,19 +3,17 @@ package TestingTest;
 public class StringSum {
 
 	public static void main(String[] args) {
-		String s ="Welcome 123 we 236";
-		int sum =0;
+		String s ="akshay";
 		for(int i=0;i<s.length();i++) {
-			if(s.charAt(i)>=48 && s.charAt(i)<=57) {
-				int n = s.charAt(i)-48;
-				sum = sum+n;
+			if(i%2==0) {
+				char ch = (char)(s.charAt(i)-32);
+				System.out.print(ch);
+			}else {
+				System.out.print(s.charAt(i));
 			}
-			
-			
-			
 		}
-		System.out.print(sum);
-
+		
+		
 	}
 
 }
