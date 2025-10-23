6d39e31cbb83232aa46f7bf3baee3a84c95ed34a
gitHaq2531 <afzaulhaque1@gmail.com>
2025-10-23 12:11:51 +0530

    c1

--- src/test/java/TestingTest/data.java
+++ src/test/java/TestingTest/data.java
@@ -5,6 +5,8 @@ public class data {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello ");
+System.out.println("TL updates");
+System.out.println("other tewsters update");
 	}
 
 }
